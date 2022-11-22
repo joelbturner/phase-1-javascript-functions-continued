ccd839d73c66e9e7d17866b0e7e01711cc665f78
Joel Turner <joelbturner.mb@gmail.com>
2022-11-21 23:59:38 -0500

    commit tree 633691010b31d1d29e5a1e4e262209fbf877e891

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return 'This Saturday, I want to ' + activity +'!';
+}
+
+function mondayWork(activity = 'go to the office'){
+    return 'This Monday, I will ' + activity +'.';
+}
+
+
+function wrapAdjective(adjective = '*'){
+    return function(special = 'special'){
+        return `You are ${adjective}${special}${adjective}!`;
+    }
+  
+}
+// function outer(greeting, msg = "It's a fine day to learn") {
+ 
+//   const innerFunction = function (name, lang = "Python") {
+
+//     return `${greeting}, ${name}! ${msg} ${lang}`; // 4
+//   };
+//   return innerFunction("student", "JavaScript"); // 5
+// }
+
+// outer("Hello");
